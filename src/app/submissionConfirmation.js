606ff4b33cd4bf68dd1e9529bad9e4929610bbb4
mmenschig <marian.menschig@googlemail.com>
2024-08-28 15:13:22 -0600

    added dotenv support to pull credentials from .env file

---
+++ src/app/submissionConfirmation.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+// TODO: This should be the page to display after a TF verification request
+// has been submitted. There should be an option to return to the previous page
+// and submit another entry.
+export default function NextPage() {
+  return (
+    <div>
+      <h1>Hello, User!</h1>
+      <p>This is a simple Next.js page.</p>
+      <p>Submit another verification request here.</p>
+    </div>
+  );
+}
